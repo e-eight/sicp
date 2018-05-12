4f1050abf2326762a7acd0754441a2f165454847
soham <soham@posteo.net>
2018-05-12 15:02:30 -0500

    no md

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SICP soutions
-I am now working through the second edition of Structure and Interpretation of Computer Programs. These are my solutions to the problems in the book. I am writing them in a literate style using [lp.scm](https://github.com/dpwright/lp.scm).
+I am now working through the second edition of Structure and
+Interpretation of Computer Programs. These are my solutions
+to the problems in the book.
---
+++ chap1/1_12.scm
@@ -0,0 +1,28 @@
+;Pascal's triangle
+;      1
+;     1 1
+;    1 2 1
+;   1 3 3 1
+;  1 4 6 4 1
+;------------
+;
+;We can restructure this as a table.
+;
+;-----------
+;|1| | | | |
+;|1|1| | | |
+;|1|2|1| | |
+;|1|3|3|1| |
+;|1|4|6|4|1|
+;-----------
+;
+;The table form makes it quite clear that the elements of the
+;triangle are given by
+;f(row, column) = 1, if column=1 or row=column,
+;f(row, column) = f(row-1, column-1) + f(row-1, column),
+;otherwise.
+
+(define (pascals-triangle row column)
+  (cond ((or (= column 1) (= row column)) 1)
+	(else (+ (pascals-triangle (- row 1) (- column 1))
+		 (pascals-triangle (- row 1) column)))))
